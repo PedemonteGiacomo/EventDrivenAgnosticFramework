532dcb106cc7fc0a50fca32957ee0336af19e2b6
Giacomo Pedemonte <giacomo.pedemonte@esaote.com>
2025-05-29 16:40:35 +0200

    feat: add jest.setup.js for global TextEncoder and TextDecoder support

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ export default {
   roots: ["packages"],
   testMatch: ["**/__tests__/**/*.test.ts"],
   transform: { "^.+\\.tsx?$": ["ts-jest"] },
-  testEnvironment: "jest-environment-jsdom"
+  testEnvironment: "jest-environment-jsdom",
+  setupFiles: ['<rootDir>/jest.setup.js']
 };
